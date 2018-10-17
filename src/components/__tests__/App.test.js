295add105e1674ea8f9e9711c107272054d14062
zlaval <zlaval@gmail.com>
2018-10-17 17:07:17 +0200

    add enzyme tests

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../App'
-
+import {shallow} from 'enzyme'
+import App from 'components/App'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
 
 it('shows a comment box', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
+    const wrapped = shallow(<App/>);
+    expect(wrapped.find(CommentBox).length).toEqual(1)
+});
 
-    expect(div.innerHTML).toContain('Comment Box');
-
-    ReactDOM.unmountComponentAtNode(div)
+it('shows a comment list', () => {
+    const wrapped = shallow(<App/>);
+    expect(wrapped.find(CommentList).length).toEqual(1)
 });
