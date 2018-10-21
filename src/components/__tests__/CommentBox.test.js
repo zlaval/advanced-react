8bec06b96b280f768745673e52be080c68b1a519
zalan <zlaval@gmail.com>
2018-10-21 16:58:29 +0200

    add textarea test

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,25 @@ it('has a text area and a button', () => {
     expect(wrapped.find('textarea').length).toEqual(1);
     expect(wrapped.find('button').length).toEqual(1);
 });
+
+it('has a text area that users can type in', () => {
+    const comment = 'new comment';
+    wrapped.find('textarea').simulate('change', {
+        target: {value: comment}
+    });
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual(comment);
+});
+
+it('when form is submitted, text area gets emptied', () => {
+    const comment = 'new comment';
+    wrapped.find('textarea').simulate('change', {
+        target: {value: comment}
+    });
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual(comment);
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+});
